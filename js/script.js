430173377f6c45a8261787b2d286cc59feab7514
slavaroskoshnyy <slava.roskoshnyy@gmail.com>
2023-03-14 12:13:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const formNode = document.querySelector('#form');
+const inputNode = document.querySelector('.js-search-input');
+const resultNode = document.querySelector('.js-result');
+
+inputNode.addEventListener("input", function (	) {
+	if (inputNode.validity.tooShort) {
+		inputNode.setCustomValidity("Введи больше :-)");
+	} else {
+		inputNode.setCustomValidity("");
+	}
+ });
+
+formNode.addEventListener('submit', async (e) => {
+	e.preventDefault();
+
+	const inputsValue = Object.fromEntries(new FormData(e.target))
+	const response = await fetch(`
+		https://api.github.com/search/repositories?q=${inputsValue.name}
+	`)
+
+	const data = await response.json();
+
+	if (response.ok && data.items.length != 0 ){
+		resultNode.classList.remove('error');
+		resultNode.innerHTML = "";
+		resultNode.appendChild(createProfile(data));
+	} else {
+		resultNode.classList.add('error');
+		resultNode.innerHTML = "Ничего не найдено";	
+	}
+	
+	inputNode.value = '';
+})
+
+function createProfile(profileData){
+	const elem = document.createElement ('div');
+	let out = ''
+	for (let i = 0; i < profileData.items.length; i++) {
+		const elem = profileData.items[i];
+			out += `
+		<div class="profile">
+			<img class="search-img" src=${elem.owner.avatar_url}></img>	
+			<p class="search-texst"><span>Имя: </span><a href="${elem.html_url}" target="_blank">${elem.name}</a></p>
+			<p class="search-texst"><span>Описание: </span>${elem.description}</p>
+			<p class="search-texst"><span>Дата создания: </span>${elem.created_at} </p>
+			<p class="search-texst"><span>Дата последнего обновления: </span>${elem.updated_at}</p>
+		</div>	
+	`		
+	 if (i == 9) break
+	}
+
+	elem.innerHTML = out;
+	
+	elem.appendChild(createDeleteBtn())
+
+	return elem
+}
+
+function createDeleteBtn(){
+	const elem = document.createElement('button');
+	elem.classList.add('delete-btn', 'btn');
+	elem.innerText = 'Удалить';
+	elem.addEventListener ('click', (e) =>{
+		resultNode.innerHTML = '';
+	});
+	return elem
+}
